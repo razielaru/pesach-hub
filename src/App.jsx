49be95a83475ac17addaaa9fbdb9dd23c3d35e5e
razielaru <razielarusiidfrav@gmail.com>
2026-03-10 23:14:30 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import { useStore } from './store/useStore'
+import LoginPage from './pages/LoginPage'
+import AppShell from './pages/AppShell'
+import Toast from './components/ui/Toast'
 
+export default function App() {
+  const currentUnit = useStore(s => s.currentUnit)
+
+  return (
+    <>
+      {!currentUnit ? <LoginPage /> : <AppShell />}
+      <Toast />
+    </>
+  )
+}
